4224d45512f73e995fcecc0451e82529cabd2e33
Sigurd Meldgaard <sigurdm@google.com>
2018-09-07 15:35:35 +0200

    WIP

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,30 @@
 import 'package:protoshaker/protos/protos.pb.dart' as p;
 
+
+class G {
+  int a = 0;
+
+  copyWith(void Function(G) updates) {
+    print("${updates.runtimeType}");
+    updates(this);
+    updates(new G());
+  }
+}
+
+class C extends G {
+  int b =0;
+  @override
+  copyWith(void Function(C) updates) {
+    print("C: ${updates.runtimeType}");
+    return super.copyWith((g) => updates(g));
+  }
+
+}
+
 main() {
-  var s = p.Sample()..duration=2;
-  print(s.duration);
+  var s = p.Response()..request=p.Request();
+  print(s.request);
+
+  C c1 = new C()..copyWith((C c) => c.b++);
+  print("${c1.a} ${c1.b}");
 }
